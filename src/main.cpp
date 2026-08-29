d427e67ed6c8906b90f7a679eb3d0bc8044e275b
Matthew2025-byte <mdstapleton2020@outlook.com>
2026-08-29 13:54:04 -0700

    Add GarnetEngine driven multithreading

--- src/main.cpp
+++ src/main.cpp
@@ -17,21 +17,15 @@ struct velocity { float x; float y; };
 struct AppState {
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
-    SDL_Thread* updateThread = nullptr;
 
-    Garnet::TextureManager textureManager;
+    Garnet::textureManager textureManager;
     Garnet::Renderer Renderer;
-
-    ThreadData threadData;
-    Garnet::Registry* regA;
-    Garnet::Registry* regB;
-    SDL_Mutex* mutexA = nullptr;
-    SDL_Mutex* mutexB = nullptr;
-    SDL_AtomicInt renderReg;
-    SDL_AtomicInt running;
+    Garnet::Registry registry;
+    Garnet::Scene scene;
+    Garnet::sceneManager manager;
 
     AppState(SDL_Window* w, SDL_Renderer* r) :
-        window(w), renderer(r), textureManager(r), Renderer(r, textureManager) {}
+        window(w), renderer(r), manager(r), textureManager(r), Renderer(r, textureManager) {}
 };
 
 
@@ -47,14 +41,11 @@ int SDLCALL ThreadLogic(void* args) {
     Uint64 last_ticks = SDL_GetTicksNS();
     while (SDL_GetAtomicInt(data.running)) {
         Uint64 frame_start = SDL_GetTicksNS();
-        // Get the renderRegly rendering buffer
         int activeBuff = SDL_GetAtomicInt(data.renderReg);
         if (activeBuff == -1) { break; };
-        Garnet::Registry& buff = *data.registries[!activeBuff];
-        // Calculate dt
-        Uint64 current_ticks = SDL_GetTicksNS();
-        float dt = (current_ticks - last_ticks) / 1'000'000'000.f;
-        last_ticks = current_ticks;
+
+        float dt = (frame_start - last_ticks) / 1'000'000'000.f;
+        last_ticks = frame_start;
 
         lastBuffer.each<Garnet::Components::Transform, velocity>([&](Garnet::Entity entity, Garnet::Components::Transform& transform, velocity& vel) {
             transform.position.x -= vel.x * dt;
@@ -64,8 +55,6 @@ int SDLCALL ThreadLogic(void* args) {
         SDL_LockMutex(data.mutexes[!activeBuff]);
         *data.registries[!activeBuff] = lastBuffer;
         SDL_UnlockMutex(data.mutexes[!activeBuff]);
-
-        // DO NOT WRITE TO THE BUFFER AFTER THIS POINT
         SDL_SetAtomicInt(data.renderReg, !activeBuff);
 
         //SDL_Log("Physics update: %d", update_count++);
@@ -80,7 +69,10 @@ int SDLCALL ThreadLogic(void* args) {
     return 0;
 }
 
-
+void updatePos(float dt, Garnet::Entity _, Garnet::Components::Transform& transform, velocity& vel) {
+    transform.position.x -= vel.x * dt;
+    transform.position.y -= vel.y * dt;
+}
 
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
@@ -97,33 +89,21 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
     AppState* app = new AppState{window, renderer};
     *appstate = app;
     
-    app->regA = new Garnet::Registry();
-    app->regB = new Garnet::Registry();
-    app->mutexA = SDL_CreateMutex();
-    app->mutexB = SDL_CreateMutex();
-    SDL_SetAtomicInt(&app->renderReg, 0);
-    SDL_SetAtomicInt(&app->running, 1);
-    
 
-    Garnet::Entity player = app->regA->createEntity();
-    app->regA->addComponent<Garnet::Components::Transform>(player, { Garnet::vec2(400, 300) });
-    app->regA->addComponent<velocity>(player, { 0, 10 });
+    Garnet::Entity player = app->registry.createEntity();
+    app->registry.addComponent<Garnet::Components::Transform>(player, { Garnet::vec2(400, 300) });
+    app->registry.addComponent<velocity>(player, { 0, 10 });
     
     SDL_PropertiesID props = SDL_CreateProperties();
     SDL_SetFloatProperty(props, GARNET_SVG_RASTER_WIDTH, 64);
-    app->regA->addComponent<Garnet::TextureID>(player, app->textureManager.Load("Eagle.svg", props));
+    app->registry.addComponent<Garnet::TextureID>(player, app->textureManager.Load("Eagle.svg", props));
     SDL_DestroyProperties(props);
-    
-    *app->regB = *app->regA;
-    app->threadData = { { app->regA, app->regB }, { app->mutexA, app->mutexB }, &app->renderReg, &app->running };
 
-    //app->scene.bind(updatePos);
-    SDL_Log("Load thread");
-    app->updateThread = SDL_CreateThread(ThreadLogic, "Update Logic", &app->threadData);
-    if (!app->updateThread) {
-        SDL_Log("SDL_CreateThread failed: %s", SDL_GetError());
-    }
-    SDL_Log("Created Thread");
+
+    app->scene.bind(updatePos);
+    app->scene.getInitRegistry() = app->registry;
+    app->manager.setActiveScene(&app->scene);
+    app->manager.start();
 
     return SDL_APP_CONTINUE;
 }
@@ -131,18 +111,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
 SDL_AppResult SDL_AppIterate(void *appstate) {
     AppState& app = *static_cast<AppState*>(appstate);
     
-
-    int index = SDL_GetAtomicInt(&app.renderReg);
-    if (index == 0) {
-        SDL_LockMutex(app.mutexA);
-        app.Renderer.update(*app.regA);
-        SDL_UnlockMutex(app.mutexA);
-    }
-    else {
-        SDL_LockMutex(app.mutexB);
-        app.Renderer.update(*app.regB);
-        SDL_UnlockMutex(app.mutexB);
-    }
+    app.Renderer.update(app.manager.getRenderRegistry());
 
     return SDL_APP_CONTINUE;
 }
@@ -156,18 +125,10 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     AppState& app = *static_cast<AppState*>(appstate);
-
-    if (app.updateThread) {
-        SDL_SetAtomicInt(&app.running, 0);
-        SDL_WaitThread(app.updateThread, nullptr);
-    }
-    SDL_DestroyMutex(app.mutexA);
-    SDL_DestroyMutex(app.mutexB);
+    
 
     if (app.renderer)          { SDL_DestroyRenderer(app.renderer); }
     if (app.window)            { SDL_DestroyWindow(app.window); }
-    delete app.regA;
-    delete app.regB;
     delete &app;
 
     return;
